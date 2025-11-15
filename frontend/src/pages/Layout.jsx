effed458b0ad7693c076902b42a3e85d6caf723d
Amilcar <bambino.amil.27@gmail.com>
2025-11-14 19:51:47 -0500

    Layout para todos los componentes

---
+++ frontend/src/pages/Layout.jsx
@@ -0,0 +1,22 @@
+import styles from "../styles/components/Layout.module.css";
+
+export default function Layout({ children }) {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header}>
+                <img src="/logo_izquierdo.png" alt="Logo Izquierdo" className={styles.logo} />
+                <h1 className={styles.title}>SISTEMA DE TUTORÍAS UNSAAC</h1>
+                <img src="/logo_derecho.png" alt="Logo Derecho" className={styles.logo} />
+            </header>
+
+            <main className={styles.main}>
+                {children}
+            </main>
+
+            <footer className={styles.footer}>
+                © 2025 Universidad Nacional de San Antonio Abad del Cusco — Todos los
+                derechos reservados.
+            </footer>
+        </div>
+    );
+}
