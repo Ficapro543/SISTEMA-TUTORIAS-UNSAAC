bd69bf68f46bf8ea112245a44d2367bb43352c47
Amilcar <bambino.amil.27@gmail.com>
2025-12-01 22:01:29 -0500

    Creando ruta para la pagina principal y comun de los roles

---
+++ frontend/src/pages/MainPageRoles.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function MainPageRoles() {
+  return (
+    <h2>Pagina principal donde todos los roles serán redireccionados despues de un login correcto</h2>
+  );
+}
+
+export default MainPageRoles;
