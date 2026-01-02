6ed32d3bf86695b30a2eeaf253b829ebc821fa1a
211265-cyber <211265@unsaac.edu.pe>
2026-01-01 21:24:25 -0500

    MIAU_2

--- backend/src/routes/verificador.js
+++ backend/src/routes/verificador.js
@@ -2,8 +2,27 @@ const express = require('express');
 const router = express.Router();
 const requireVerifier = require('../middleware/requireVerifier');
 
-const { getEstudiantesPorSemestreEstado } = require('../controllers/verificadorController');
+const {
+    getEstudiantesPorSemestreEstado,
+    getTutoriasPorSemestre,
+    buscarEstudiante,
+    getHistorialEstudiante,
+    getTutores,
+    getSeguimientoTutor
+} = require('../controllers/verificadorController');
 
+// HU-VER-01
 router.get('/estudiantes', requireVerifier, getEstudiantesPorSemestreEstado);
+
+// HU-VER-02
+router.get('/tutorias', requireVerifier, getTutoriasPorSemestre);
+
+// HU-VER-03
+router.get('/estudiantes/buscar', requireVerifier, buscarEstudiante);
+router.get('/estudiantes/:codigo/historial', requireVerifier, getHistorialEstudiante);
+
+// HU-VER-04
+router.get('/tutores', requireVerifier, getTutores);
+router.get('/tutores/:id/seguimiento', requireVerifier, getSeguimientoTutor);
 
 module.exports = router;
