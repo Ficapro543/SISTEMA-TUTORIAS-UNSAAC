46bf08eb12e7e32d00f7c25a7a42f8f2ec070e24
Amilcar <bambino.amil.27@gmail.com>
2026-01-06 02:49:10 -0500

    CORS exception solved?

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,23 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3001',
+  process.env.FRONTEND_URL, // Deployed Frontend URL from ENV
+].filter(Boolean);
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log('Origin blocked by CORS:', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
